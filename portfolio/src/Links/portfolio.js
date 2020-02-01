36ed606eab0516ed5ad7985f0dd61d8fcd294552
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-02-01 15:24:28 +0000

    Fixed image mapping bug in portfolio folder Got download pdf working on resume page installed font awesome icons

--- portfolio/src/Links/portfolio.js
+++ portfolio/src/Links/portfolio.js
@@ -2,7 +2,7 @@ import React from "react";
 import SideBar from "./sidebar";
 import images from "./images";
 
-import Lightbox from 'react-lightbox-component';
+import Lightbox from "react-lightbox-component";
 // import styled from 'styled-components';
 export default function Portfolio() {
   return (
@@ -10,28 +10,20 @@ export default function Portfolio() {
       <SideBar />
       <div id="page-wrap2">
         <h1>Portfolio</h1>
-        {/* {images.map(({ id, src, title, description }) => (
-          <img 
-          key= { id }
-          src= { src } 
-          title= { title } 
-          alt= { description } 
-          />
-        ))} */}
-        {/* <Lightbox images={images} 
-        thumbnailWidth='300px'
-        thumbnailHeight='300px'/>;/> */}
         <Lightbox
-  images={images}
-  renderImageFunc={(idx, image, toggleLightbox) => {
-    return (
-      <img
-        key={idx}
-        src={image.src}
-        style={{width: '250px', height: '250px', margin: '10px'}}
-        onClick={toggleLightbox.bind(null, idx)} />
-    )
-  }}/>
+          images={images}
+          renderImageFunc={(idx, image, title, description) => {
+            return (
+              <img
+                key={idx}
+                src={image.src}
+                style={{ width: "250px", height: "200px", margin: "10px"}}
+                title= { title }
+                alt= { description }
+              />
+            );
+          }}
+        />
       </div>
     </div>
   );
