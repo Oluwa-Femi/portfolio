f45089041f76734d222f89d8e0a11aead5f8bcca
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-02-02 22:21:12 +0000

    refactoring

--- portfolio/src/Links/connect.js
+++ portfolio/src/Links/connect.js
@@ -17,34 +17,36 @@ export default function Connect() {
       <SideBar />
       <div id="page-wrap2">
         <h1>Connect</h1>
+        <h2>Say hello! Will ya?</h2>
+        <p>I live on linkedin and twitter, My git is always online, I haven't touched Instagram in 3 years</p>
         <div>
           <a
             href="https://www.linkedin.com/in/oluwafemi-oluwatola/"
             className="linkedin social"
             target = "_blank"
           >
-            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            <FontAwesomeIcon icon={faLinkedin} size="3x" />
           </a>
           <a
             href="https://github.com/Oluwa-Femi/"
             className="github social"
             target = "_blank"
           >
-            <FontAwesomeIcon icon={faGithub} size="2x" />
+            <FontAwesomeIcon icon={faGithub} size="3x" />
           </a>
           <a
             href="https://twitter.com/femi_oluwatola"
             className="twitter social"
             target = "_blank"
           >
-            <FontAwesomeIcon icon={faTwitter} size="2x" />
+            <FontAwesomeIcon icon={faTwitter} size="3x" />
           </a>
           <a
             href="https://www.instagram.com/femi_oluwatola/"
             className="instagram social"
             target = "_blank"
           >
-            <FontAwesomeIcon icon={faInstagram} size="2x" />
+            <FontAwesomeIcon icon={faInstagram} size="3x" />
           </a>
         </div>
       </div>
--- portfolio/src/Links/portfolio.js
+++ portfolio/src/Links/portfolio.js
@@ -10,6 +10,8 @@ export default function Portfolio() {
       <SideBar />
       <div id="page-wrap2">
         <h1>Portfolio</h1>
+        <p>I spend a lot of time on the keyboard birthing apps and websites.</p>
+        <p>I have the source code <a href="https://github.com/Oluwa-Femi">here</a></p>
         <Lightbox
           images={images}
           renderImageFunc={(idx, image, title, description) => {
@@ -17,7 +19,7 @@ export default function Portfolio() {
               <img
                 key={idx}
                 src={image.src}
-                style={{ width: "250px", height: "200px", margin: "10px"}}
+                style={{ width: "200px", height: "200px", margin: "10px"}}
                 title= { title }
                 alt= { description }
               />
