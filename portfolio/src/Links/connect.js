63b214f4b82911c3fcaea8365794597bf4e8b9ba
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-02-07 22:35:02 +0000

    formik installation

--- portfolio/src/Links/connect.js
+++ portfolio/src/Links/connect.js
@@ -57,16 +57,7 @@ export default function Connect() {
           </a>
         </div>
         <div class="form">
-          <Form>
-            <Form.Group controlId="exampleForm.ControlInput1">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="name@example.com" />
-            </Form.Group>
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-              <Form.Label>Example textarea</Form.Label>
-              <Form.Control as="textarea" rows="3" />
-            </Form.Group>
-          </Form>
+          
         </div>
       </div>
     </div>
