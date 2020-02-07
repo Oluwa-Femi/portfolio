52e4a4ea85de9de373d24e620cb84a34b177740d
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-02-07 08:01:00 +0000

    contact form

--- portfolio/src/Links/connect.js
+++ portfolio/src/Links/connect.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SideBar from "./sidebar";
+import { Form } from 'react-bootstrap';
 
 import "../App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -54,6 +55,18 @@ export default function Connect() {
           >
             <FontAwesomeIcon icon={faInstagram} size="3x" />
           </a>
+        </div>
+        <div class="form">
+          <Form>
+            <Form.Group controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="name@example.com" />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Example textarea</Form.Label>
+              <Form.Control as="textarea" rows="3" />
+            </Form.Group>
+          </Form>
         </div>
       </div>
     </div>
